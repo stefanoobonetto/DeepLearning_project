eca884fa30788aa33a8825231780f7b5d1890486
bonnie <stefanoobonetto@gmail.com>
2024-05-28 14:11:22 +0200

    png comparison

--- analize_csv.py
+++ analize_csv.py
@@ -18,34 +18,41 @@ def leggi_csv(file_path):
     return dati
 
 # Esempio di utilizzo
-file_path = '/home/sagemaker-user/DeepLearning_project/accuracy_results.csv'
+file_path = '/home/bonnie/Desktop/deep_learning/project/DeepLearning_project/test_1_all_dataset_RESNET50 (1).csv'
 dati = leggi_csv(file_path)
 
 dati_before_MEMO = []
 dati_after_MEMO = []
+dati_after_MEMO_PLUS = []
 
-for riga in dati:
-    split_name = riga['class'].split('_')
-    if split_name[1] == "before":
-        dati_before_MEMO.append(float(riga['accuracy']))
-    elif split_name[1] == "after":
-        dati_after_MEMO.append(float(riga['accuracy']))
+for index, riga in enumerate(dati):
+    if index != 0 and index != len(dati) - 1:
+        split_name = riga['class'].split('_')
+        print(split_name)
+        if split_name[1] == "before":
+            dati_before_MEMO.append(float(riga['accuracy']))
+        elif split_name[1] == "after":
+            if len(split_name) == 3:
+                dati_after_MEMO_PLUS.append(float(riga['accuracy']))
+            else:
+                dati_after_MEMO.append(float(riga['accuracy']))
 
 # Creazione del plot
 plt.figure(figsize=(10, 6))
 
 # Assumendo che i dati siano ordinati per classe o in ordine temporale
-plt.plot(dati_before_MEMO[:50], label='Before MEMO', marker='o')
-plt.plot(dati_after_MEMO[:50], label='After MEMO', marker='o')
+plt.plot(dati_before_MEMO, label='Before MEMO', marker='o')
+plt.plot(dati_after_MEMO, label='After MEMO', marker='o')
+plt.plot(dati_after_MEMO_PLUS, label='After MEMO_PLUS', marker='o')
 
 # Aggiungere etichette e titolo
-plt.xlabel('Indice')
+plt.xlabel('Class')
 plt.ylabel('Accuracy')
 plt.title('Accuracy Before and After MEMO')
 plt.legend()
 
 # Salva il grafico in un file
-output_path = '/home/sagemaker-user/DeepLearning_project/accuracy_plot.pdf'
+output_path = 'accuracy_plot.pdf'
 plt.savefig(output_path)
 
 # Mostrare il grafico
